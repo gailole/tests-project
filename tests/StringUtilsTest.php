2b463820062d998f80053745dffad0fb9ac9647c
gailole <oleg.gailun@yandex.ru>
2023-05-16 11:33:37 +0400

    add composer.json, fix StringUtilsTest.php

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,1 +1,12 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use function Tests\Project\StringUtils\capitalize;
+if (capitalize('hello') !== 'Hello') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+    throw new \Exception('Функция работает неверно!');
+}
+
+echo 'Все тесты пройдены!';
