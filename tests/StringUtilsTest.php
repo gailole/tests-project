58273f1d54c7b0ab4df39c175429d5389a1dd4f8
gailole <oleg.gailun@yandex.ru>
2023-05-16 12:39:39 +0400

    add webmozart/assert in composer.json, exchange StringUtilsTest.php

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,12 +1,19 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 use function Tests\Project\StringUtils\capitalize;
-if (capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+use Webmozart\Assert\Assert;
 
-if (capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+//if (capitalize('hello') !== 'Hello') {
+//    throw new \Exception('Функция работает неверно!');
+//}
+//
+//if (capitalize('') !== '') {
+//    throw new \Exception('Функция работает неверно!');
+//}
+
+Assert::eq(capitalize('hello'), 'Hello');
+Assert::eq(capitalize(''), '');
+
 
 echo 'Все тесты пройдены!';
+
